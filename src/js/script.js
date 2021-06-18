e8ac51ab413338ac95e5af3ac592c8712c02040f
Pavel Lobarev <pavellobarev@yahoo.com>
2021-06-18 12:34:19 +0300

    Added footer

--- src/js/script.js
+++ src/js/script.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+        // responsive: [
+        //     {
+        //         breakpoint: 992,
+        //         settings: {
+        //             dots: true,
+        //             arrows: false
+        //         }
+        //     }
+        // ]
     });
     
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
